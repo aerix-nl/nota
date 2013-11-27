dc735ac1fda045aaa334c711d86d9606e690b5cd
Felix Akkermans <felix.akkermans@gmail.com>
2013-11-27 20:34:16 +0100

    Kinda broken setup and compiling now
    But it's moving towards Linux support, and hassle/CLI free compiling, managed by Node.js automagically

--- setup.sh
+++ setup.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 # Call this script from root of Nota directory
 
-# Ensure both PhantomJS, CoffeeScript, SASS and Bower have been installed by Node Package Manager
+# We need PhantomJS either though something like brew, apt-get or yum
+brew install phantomjs
+
+# Ensure both we have our serverside dependencies installed by the Node Package Manager
 npm install phantom
 npm install coffee-script
-npm install sass
+npm install node-sass
 npm install -g bower
+npm install require
+npm install node-compass
+
+
 
 # Get our clientside dependencies (jQuery, Backbone.js, Underscore.js, Bootstrap, FontAwesome etc.)
-bower install
+node_moduplus/bower/bin/bower install
 
 # First we ensured all neccessary files are compiled down to native form.
 # Add --watch to the options start a process that listens to the filesystem changes and
 # automatically recompiles on change events.
-coffee --compile javascript/*.coffee
+node_modules/coffee-script/bin/coffee --compile javascript/*.coffee
 ./compile-example-template.sh
