f671c81247533c77093b8b3b6f3cd8482508e2c6
Felix Akkermans <felix.akkermans@gmail.com>
2013-11-29 18:10:58 +0100

    Better setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 # Call this script from root of Nota directory
 
-# We need PhantomJS either though something like brew, apt-get or yum
-brew install phantomjs
+# We need PhantomJS 1.9 or greater though something like brew, apt-get or yum
+# On ElementaryOS Luno / Ubuntu 12.04 this means you'll have to do the following:
+# cd /usr/local/share/
+# sudo wget https://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-x86_64.tar.bz2
+# sudo tar xjf phantomjs-1.9.2-linux-x86_64.tar.bz2
+# sudo ln -s /usr/local/share/phantomjs-1.9.2-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs
+# sudo ln -s /usr/local/share/phantomjs-1.9.2-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
+# sudo ln -s /usr/local/share/phantomjs-1.9.2-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
+sudo apt-get install phantomjs
+
+# Also we need NodeJS 0.10 or greater
+# On ElementaryOS Luno / Ubuntu 12.04 this means you'll have to do the following:
+# sudo apt-get update
+# sudo apt-get install -y python-software-properties python g++ make
+# sudo add-apt-repository -y ppa:chris-lea/node.js
+# sudo apt-get update
+sudo apt-get install nodejs
 
 # Ensure both we have our serverside dependencies installed by the Node Package Manager
 npm install phantom
@@ -11,8 +26,6 @@ npm install node-sass
 npm install -g bower
 npm install require
 npm install node-compass
-
-
 
 # Get our clientside dependencies (jQuery, Backbone.js, Underscore.js, Bootstrap, FontAwesome etc.)
 node_moduplus/bower/bin/bower install
